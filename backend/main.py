37d70afdf7258b58d7a91755cf1a19e70429859b
Walter-D3v <walterantoniomelendez10@gmail.com>
2025-06-25 00:13:03 -0600

    listening ports

--- backend/main.py
+++ backend/main.py
@@ -27,3 +27,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+uvicorn.run("main:app", host="0.0.0.0", port=10000)
